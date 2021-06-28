7701286941369d11dc54541919ce7f2ee64e718e
QAQmmttyyy <miaotianyugo@163.com>
2021-06-28 16:10:16 +0800

    Update test.

--- src/__tests__/BillList.test.tsx
+++ src/__tests__/BillList.test.tsx
@@ -2,42 +2,34 @@ import React from "react";
 import { rest } from "msw";
 import { act, render, screen, waitFor } from "@testing-library/react";
 import { server } from "../apiMocks/server";
+import { fakeIncomeBill, fakeIncomeCategory } from "../apiMocks/fakeData";
 import {
   BASE_URL,
   BillSearchParamsKey,
   CNY_SYMBOL,
   INCOME_TEXT,
 } from "../constants";
-import { Bill, BillCategory } from "../types";
+import { Bill } from "../types";
 import { useApiStore } from "../store/api.store";
 import { useUrlSearchParamsStore } from "../store/urlSearchParams.store";
 import BillList from "../containers/BillList";
 
 describe("<BillList />", () => {
-  const fakeDate = new Date(2021, 5, 7, 9, 30);
-  const fakeBill: Bill = {
-    type: 1,
-    time: fakeDate.getTime(),
-    category: "1",
-    amount: 1000,
-  };
-  const fakeCategory: BillCategory = {
-    id: "1",
-    type: 1,
-    name: "投资",
-  };
+  const fakeBillDate = new Date(fakeIncomeBill.time);
   const listItemTextDict = {
-    time: fakeDate.toLocaleDateString(), // "2021/6/7" for "zh-CN" locale
-    category: fakeCategory.name, // "投资"
-    description: `${INCOME_TEXT} ${CNY_SYMBOL}${fakeBill.amount.toFixed(2)}`, // "收入 ￥1000.00"
+    time: fakeBillDate.toLocaleDateString(), // "2021/6/7" for "zh-CN" locale
+    category: fakeIncomeCategory.name, // "投资"
+    description: `${INCOME_TEXT} ${CNY_SYMBOL}${fakeIncomeBill.amount.toFixed(
+      2
+    )}`, // "收入 ￥1000.00"
   };
 
   server.use(
     rest.get<undefined, Bill[]>(`${BASE_URL}/bill`, (req, res, ctx) => {
-      return res(ctx.json([fakeBill]));
+      return res(ctx.json([fakeIncomeBill]));
     })
   );
-  useApiStore.setState({ billCategories: [fakeCategory] });
+  useApiStore.setState({ billCategories: [fakeIncomeCategory] });
 
   test("renders bill list", async () => {
     // ""
@@ -62,7 +54,7 @@ describe("<BillList />", () => {
       // "year=2021"
       useUrlSearchParamsStore.setState({
         urlSearchParams: new URLSearchParams(
-          `${BillSearchParamsKey.YEAR}=${fakeDate.getFullYear()}`
+          `${BillSearchParamsKey.YEAR}=${fakeBillDate.getFullYear()}`
         ),
       });
     });
