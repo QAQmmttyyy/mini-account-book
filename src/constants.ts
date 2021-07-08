103cbf8f391dc7c29b3055a993243685434b2288
QAQmmttyyy <miaotianyugo@163.com>
2021-07-08 16:13:30 +0800

    Update consts.

--- src/constants.ts
+++ src/constants.ts
@@ -20,8 +20,8 @@ export const CHOOSE_TIME_TEXT = "选择时间";
 export const TYPE_TEXT = "类型";
 export const AMOUNT_TEXT = "金额";
 export const CATEGORY_TEXT = "分类";
-export const CREATE_BILL_TEXT = "新建账单";
-export const CONFIRM_TEXT = "confirm";
+export const CREATE_BILL_TEXT = "创建账单";
+export const CONFIRM_TEXT = "确定";
 export const CANCEL_TEXT = "取消";
 
 export enum ExtraCategoryValue {
@@ -32,10 +32,4 @@ export enum ExtraCategoryValue {
 export enum ExtraCategoryName {
   ALL = "全部分类",
   NONE = "未分类",
-}
-
-export enum BillSearchParamsKey {
-  YEAR = "year",
-  MONTH = "month",
-  CATEGORY = "category",
 }
