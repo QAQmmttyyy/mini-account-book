7266fd99b9a5713d353725e1f13441d92b8a46f0
QAQmmttyyy <miaotianyugo@163.com>
2021-07-14 22:10:11 +0800

    Add condition to mock.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { initDB } from "./apiMocks/db";
 import { worker } from "./apiMocks/browser";
 import App from "./App";
 
-initDB();
-worker.start();
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
 ReactDom.render(<App />, document.getElementById("root"));
