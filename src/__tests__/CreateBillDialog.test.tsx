73924eedddab8b4d39c851bf91ef10f790bf31e2
QAQmmttyyy <miaotianyugo@163.com>
2021-07-15 14:13:46 +0800

    Update test.

--- src/__tests__/CreateBillDialog.test.tsx
+++ src/__tests__/CreateBillDialog.test.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import userEvent from "@testing-library/user-event";
-import { render, screen, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import {
   fakeExpenditureCategory,
   fakeIncomeCategory,
@@ -9,12 +14,22 @@ import CreateBillDialog from "../components/CreateBillDialog";
 import { useApiStore } from "../store/api.store";
 import {
   AMOUNT_TEXT,
+  BASE_URL,
   CATEGORY_TEXT,
   CHOOSE_TIME_TEXT,
   CONFIRM_TEXT,
   CREATE_BILL_TEXT,
   INCOME_TEXT,
+  NOT_EMPTY_TEXT,
 } from "../constants";
+import { server } from "../apiMocks/server";
+import { Bill } from "../types";
+
+server.use(
+  rest.post<Bill, Bill>(`${BASE_URL}/bill`, (req, res, ctx) => {
+    return res(ctx.json(req.body));
+  })
+);
 
 useApiStore.setState({
   billCategories: [fakeIncomeCategory, fakeExpenditureCategory],
@@ -37,22 +52,18 @@ test("normal process", async () => {
   userEvent.type(screen.getByLabelText(new RegExp(AMOUNT_TEXT)), "99999.99");
   userEvent.click(screen.getByText(CONFIRM_TEXT));
 
-  await waitFor(() => {
-    expect(
-      screen.queryByRole("dialog", { name: CREATE_BILL_TEXT })
-    ).not.toBeInTheDocument();
-  });
+  await waitForElementToBeRemoved(
+    screen.getByRole("dialog", { name: CREATE_BILL_TEXT })
+  );
 });
 
 test("improper process", async () => {
   render(<DialogControl />);
 
   userEvent.click(screen.getByText(CONFIRM_TEXT));
-  await new Promise((resolve) => {
-    setTimeout(resolve, 1000);
-  });
 
+  expect(screen.getByText(NOT_EMPTY_TEXT)).toBeInTheDocument();
   expect(
-    screen.queryByRole("dialog", { name: CREATE_BILL_TEXT })
+    screen.getByRole("dialog", { name: CREATE_BILL_TEXT })
   ).toBeInTheDocument();
 });
